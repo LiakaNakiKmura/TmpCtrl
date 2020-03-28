6be8c68a240b4530e1fda937a9b58d610b041b76
LiNaK <k.sasa6ra.0317@gmail.com>
2020-03-28 10:38:39 +0900

    Add test for  parts

---
+++ tests/test_parts.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 00:31:58 2020
+@author: LiNaK
+"""
+# Standard module
+import unittest
+
+# 3rd party's module
+
+# Original module  
+from context import src # path setting
+from test_interface import TestForMethodExist
+from testing_utility.unittest_util import cls_startstop_msg as add_msg
+
+# target
+from src.interface.intfc_com import (Parts)
+
+@add_msg
+class TestCombinePN(TestForMethodExist, unittest.TestCase):
+    _class_method_pairs=((Parts,('get_data','set_data','get_parameter_name')),
+                         )
+
+if __name__=='__main__':
+    unittest.main() 
