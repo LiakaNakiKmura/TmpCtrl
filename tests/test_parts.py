1c099f57cb5a89399e59ce4118bca36055e806f9
LiNaK <k.sasa6ra.0317@gmail.com>
2020-03-29 20:18:35 +0900

    Add NTC class

--- tests/test_parts.py
+++ tests/test_parts.py
@@ -7,6 +7,7 @@ Created on Wed Mar 25 00:31:58 2020
 import unittest
 
 # 3rd party's module
+from numpy.testing import assert_almost_equal
 
 # Original module  
 from context import src # path setting
@@ -16,6 +17,7 @@ from testing_utility.unittest_util import cls_startstop_msg as add_msg
 
 # target
 from src.interface.intfc_com import (Parts)
+from src.parts.parts_func import (NTC)
 
 @add_msg
 class TestPartsInterFace(TestForMethodExist, unittest.TestCase):
@@ -23,9 +25,40 @@ class TestPartsInterFace(TestForMethodExist, unittest.TestCase):
                                  'get_parameter_name')),
                          )
 
+
+class ResistTestMethods():
+    _cls = Parts # targe Resist class inherit Parts class.
+    _parameter_names = ('',) # collect parameter names by get_parameter_names.
+    _paradict_val_pairs = (({'':None},0),) 
+    # tuple of pair of parameter name, value dict and collect resist value.  
+    _initial_set = {} # initial setting for target class.
+    
+    def setUp(self):
+        self.parts = self._cls(**self._initial_set)
+    
+    def test_inherite(self):
+        self.assertTrue(issubclass(self._cls, Parts))
+    
+    def test_parameter_names(self):
+        parameter_names = self.parts.get_parameter_name()
+        self.assertCountEqual(parameter_names, self._parameter_names)
+    
+    def test_get_parameter(self):
+        for paradict, val in self._paradict_val_pairs:
+            self.parts.set_parameter(**paradict)
+            assert_almost_equal(val, self.parts.get_value())
+            
+
 @add_msg
-class TestNTC(unittest.TestCase):
-    pass
+class TestNTC(ResistTestMethods, unittest.TestCase):
+    _cls = NTC
+    _parameter_names = ('temeperature_degC',)
+    _paradict_val_pairs = (({'temeperature_degC':-30},216729.001660428 ),
+                           ({'temeperature_degC':25},10000 ),
+                           ({'temeperature_degC':80},1203.301326499)
+                           )
+    _initial_set = {'Resist_at_T2':10000,'T2':25, 'B_const':4050}
+    
 
 if __name__=='__main__':
     unittest.main() 
